c9692249bcc89a0114115d803f3ccbc0acfb3a28
Genicleito Gonçalves <Genicleito@users.noreply.github.com>
2016-04-06 18:26:50 -0300

    REVISAR

--- 03_Fibonacci.lisp
+++ 03_Fibonacci.lisp
@@ -13,10 +13,10 @@
   (fib-acum n 0 1))
 
 ; testes
-(print (fibonacci(1)))
-(print (fibonacci(2)))
-(print (fibonacci(3)))
-(print (fibonacci(4)))
-(print (fibonacci(5)))
-(print (fibonacci(6)))
-(print (fibonacci(40)))
+(print (fibonacci 1))
+(print (fibonacci 2))
+(print (fibonacci 3))
+(print (fibonacci 4))
+(print (fibonacci 5))
+(print (fibonacci 6))
+(print (fibonacci 40))
