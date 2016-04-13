32314985922d990eeec2d7e59f447323aac1bf3c
Genicleito Gonçalves <Genicleito@users.noreply.github.com>
2016-04-13 18:22:17 -0300

    Usando recursão profunda.
    Crie uma função, (subst x y expr), que substitua as oorrências de x por y na expressão expr, usando recursão profunda.

--- subst-prof.lisp
+++ subst-prof.lisp
@@ -12,7 +12,7 @@
     (t (cons (car expr) (subst-prof x y (cdr expr))))))
 
 ; testes
-(print (subst-prof 1 2 '(1 2 3)))	;	2 2 3
-(print (subst-prof 1 2 '(1 (2 3))))	;	2 (2 3)
+(print (subst-prof 1 2 '(1 2 3)))	;	(2 2 3)
+(print (subst-prof 1 2 '(1 (2 3))))	;	(2 (2 3))
 (print (subst-prof 1 2 '(1 2 (1 3 2 1) 3)))	;	(2 2 (2 3 2 2) 3)
 (print (subst-prof 1 2 '(1 2 ((1 3) 2 1) 3)))	;	(1 2 ((2 3) 2 2) 3)
