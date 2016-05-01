8318726e96eecd68a7a498aea72332f71f09b65e
Genicleito <genicleito@live.com>
2016-05-01 16:33:47 -0300

    Programação Funcional - Funções de Alta Ordem

---
+++ maiorBoxArtReduce.js
@@ -0,0 +1,32 @@
+function maiorBoxArtReduce() {
+    var boxarts = [
+            { width: 200, height:200, url: "http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+            { width: 150, height:200, url: "http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
+            { width: 300, height:200, url: "http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" },
+            { width: 425, height:150, url: "http://cdn-0.nflximg.com/images/2891/Fracture425.jpg" }
+        ],
+        currentSize,
+        maxSize = -1,
+        largestBoxart;
+
+    boxarts.reduce(function(boxart1, boxart2){
+        if(boxart1.width * boxart1.height < boxart2.width * boxart2.height){
+            largestBoxart = boxart2.width * boxart2.height;
+            return boxart2;
+        }else{
+            largestBoxart = boxart1.width * boxart1.height;
+            return boxart1;
+        }
+    });
+
+/*    boxarts.forEach(function(boxart) {
+        currentSize = boxart.width * boxart.height;
+        if(currentSize > maxSize){
+            largestBoxart = currentSize;
+            maxSize = currentSize;
+        }
+    });
+*/
+    return largestBoxart;
+}
+ufbassert(63750, maiorBoxArtReduce());
